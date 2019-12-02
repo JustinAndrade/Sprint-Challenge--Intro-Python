fc27986b50a7316bd46c5c101bc5e7974f357eb5
justinandrade <lambda_school_loaner_202@Lambda-School-Loaner-202s-MacBook-Pro.local>
2019-12-02 14:59:47 -0500

    oop1 completed

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,28 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# This is the base class
+class Vehicle:
+    def __init__(self):
+
+class GroundVehicle(Vehicle):
+    super().__init__(self):
+
+class FlightVehicle(Vehicle):
+    super().__init__(self):
+
+class Airplane(FlightVehicle):
+    super().__init__(self)
+
+class Starship(FlightVehicle):
+    super().__init__(self)
+
+class Car(GroundVehicle):
+    super().__init__(self):
+
+class Motorcycle(GroundVehicle):
+    super().__init__(self):
+
+
+
